e00c7981290880f39a2ae85565a97d58ab5885aa
nikhildashore09 <nikhildashore.123@gmail.com>
2020-03-08 07:10:45 +0530

    Added specs for prize#get_winning_chance_in_percent

--- spec/models/prize_spec.rb
+++ spec/models/prize_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Prize, type: :model do
   describe 'associations' do
     it { should have_many(:user_opted_prizes) }
   end
+
+  describe '#get_winning_chance_in_percent' do
+    let(:prize) { create(:prize) }
+    it 'should return expected percents' do
+      create(:user_opted_prize, prize: prize)
+      expect(prize.get_winning_chance_in_percent).to eq(100)
+      create(:user_opted_prize, prize: prize)
+      expect(prize.get_winning_chance_in_percent).to eq(50)
+      create(:user_opted_prize, prize: prize)
+      expect(prize.get_winning_chance_in_percent).to eq(33.33)
+    end
+  end
 end
